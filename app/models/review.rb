24b185647f23dcd4d6584d8bc325003f651baec9
Rhea <rhea.morrison@gmail.com>
2017-02-17 22:00:04 +0000

    step 2

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ActiveRecord::Base
   belongs_to :product
+  validates :product_id,
+            :user_id,
+            :description,
+            :rating,
+            presence: true
 end
