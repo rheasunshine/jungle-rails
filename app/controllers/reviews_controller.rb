325f5760ae90df0ebce16ede42d3218794561324
Rhea <rhea.morrison@gmail.com>
2017-02-20 16:52:55 +0000

    fixed review bug

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,15 +7,21 @@ class ReviewsController < ApplicationController
   end
 
   def create
-    product = Product.find(params[:product_id].to_i)
+    @product = Product.find(params[:product_id].to_i)
     rating = params[:review][:rating]
     description = params[:review][:description]
     if rating && description
-      @review = Review.new(product_id: product.id, user_id: current_user.id, rating: rating.to_i, description: description)
+      @review = Review.new(product_id: @product.id, user_id: current_user.id, rating: rating.to_i, description: description)
       @review.save
-    else
-      redirect_to "/products/#{params[:product_id]}"
+      if @review.save
+        flash[:notice] = "Your review has been submitted successfully"
+        redirect_to @product
+      else
+        flash[:error] = "There was an error deleteing the purchase."
+        render :show
+      end
   end
+end
 
   def destroy
     @product = Product.find(params[:product_id])
@@ -35,27 +41,3 @@ class ReviewsController < ApplicationController
     end
 
 end
-
-
-#
-#   def create
-#     @review = Review.new(review_params)
-#     if @review.save
-#
-# redirect_to("/products/:id")
-#       # respond_to do |format|
-#       #   format.html { redirect_to @review } #, notice: 'Post was successfully created'
-#       #   format.json {render :show, status: :created, location: @review }
-#       #   format.js
-#       # end
-#     end
-#   end
-#
-#   def update
-#     @review.update(description: @review.description, rating: @review.rating)
-#   end
-#
-#   def destroy
-#     @review = Review.find params[:id]
-#     @review.destroy
-#   end
