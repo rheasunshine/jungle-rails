26847b878ef303c83c929298efddb4e06c1ad95b
Rhea <rhea.morrison@gmail.com>
2017-02-22 15:22:35 +0000

    stars on product browsing page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+def average_rating_helper(product)
+  average_rating = 0
+  reviews =  Review.where(product_id: product.id)
+  if reviews.length > 0
+    average_rating = Review.where(product_id: product.id).average(:rating).round(2)
+  end
+  average_rating
+end
+
 end
